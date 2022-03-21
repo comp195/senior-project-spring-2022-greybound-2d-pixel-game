325df66c2d1b77369df48a392cb352f62f7eb603
George-Upton4 <g_upton@u.pacific.edu>
2022-03-20 23:31:10 -0700

    Edited save/load game

--- Greybound/Assets/Scripts/MainMenu.cs
+++ Greybound/Assets/Scripts/MainMenu.cs
@@ -17,4 +17,18 @@ public class MainMenu : MonoBehaviour
         Debug.Log("QUIT!");
         Application.Quit();
     }
+
+    public void Loadgame()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        /* Import all player attributes */
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+        SceneManager.LoadScene("TestLevel");
+    }
 }
